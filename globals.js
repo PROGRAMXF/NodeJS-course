a8c1157956251a6d8558c3c5ec70d0c37f8e1781
Fernando <fh1273509@mail.com>
2022-11-16 22:51:40 -0300

    objetos globales

---
+++ globals.js
@@ -0,0 +1,8 @@
+//objetos globales
+console.log(__dirname) // me tira el directorio donde me encuentro
+console.log(__filename) // me tira la ruta completa, con el archivo en el cual estoy trabajando
+
+console.log(module)//objeto que me da informacion para trabajar con multiples archivos separados
+console.log(require)// me permite importar los archivos separados de module
+
+console.log(process)// me da informacion del sistema
