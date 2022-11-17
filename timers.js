3b016d53e73e811ed032378e30e5fc0a0b79d9f6
Fernando <fh1273509@mail.com>
2022-11-17 20:07:12 -0300

    timers

---
+++ timers.js
@@ -0,0 +1,19 @@
+/*
+setInterval //ejecuta un codigo cada cierto tiempo
+setTimeout  //ejecuta un codigo despues que pasa un tiempo
+*/
+
+
+//ejemplo
+/*
+setInterval(()=>{
+    console.log("hola mundo");
+
+},2000)
+*/
+
+
+setTimeout(()=> {
+    console.log("hola mundo");
+
+},2000)
