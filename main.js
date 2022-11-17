91b2976d2f3bbd8839afc95d4c77c58a6535d246
Fernando <fh1273509@mail.com>
2022-11-17 20:55:09 -0300

    module export e import

---
+++ main.js
@@ -0,0 +1,5 @@
+
+//ahora extraemos los valores exportados en exports
+const web = require('./module/myModule');
+
+console.log(web);
--- module/myModule.js
+++ module/myModule.js
@@ -1,9 +1,16 @@
 //los modulos se usan para aplicaciones grandes
 
-const myWebAddress = "infobae.com";
+const myWebAddress = "programxf";
+const myNumber = 50;
+const myName = "Fernando";
+const myLastName = "Herrera";
 
 //si esta variable  const la quiero reutilizar en el resto de la aplicacion la tengo que exportar
 
-module.exports = myWebAddress;
-
-console.log(module);
+const group = {
+    myWebAddress: myWebAddress,
+    myNumber: myNumber,
+    myName: myName,
+    myLastName: myLastName
+}
+module.exports = group;
