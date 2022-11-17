0bfeaf0db66ab94a1c68da88ea7878aa90837531
Fernando <fh1273509@mail.com>
2022-11-17 20:25:04 -0300

    exportar una variable

---
+++ module/myModule.js
@@ -0,0 +1,9 @@
+//los modulos se usan para aplicaciones grandes
+
+const myWebAddress = "infobae.com";
+
+//si esta variable  const la quiero reutilizar en el resto de la aplicacion la tengo que exportar
+
+module.exports = myWebAddress;
+
+console.log(module);
