d17e9c78d7becac8a94c5dbe44238b7f47a05c4c
Fernando <fh1273509@mail.com>
2022-11-16 22:31:20 -0300

    primeros apuntes de codigo..funciones..bucles..condicionales..etc

---
+++ index.js
@@ -0,0 +1,56 @@
+/*let username = 'fernando';
+let age = 50;
+let hasHobbies = false;
+let points = [10, 20, 30]; //array
+
+//objeto
+let user = {
+    name: 'fer', 
+    lastName: 'herrera'
+}
+
+const PI = 3.1416
+
+console.log(username)
+console.log(age)
+console.log(points)
+console.log(user)
+
+
+const edad = 50;
+
+if(edad > 45){
+    console.log('tu eres un adulto')
+}
+else{
+    console.log('eres un joven')
+}
+*/
+
+/*
+//bucles
+
+const names = ['jose', 'fer', 'marcos'];
+
+for( let i = 0; i < names.length; i++){
+    console.log(names[i]);
+}*/
+
+//funciones
+/*
+function ShowUserInfo(userName, userAge){
+    return `the username is ${userName}, the user is ${userAge} years old`
+}
+console.log(ShowUserInfo('fer', 50))
+console.log(ShowUserInfo('marcos', 35))
+console.log(ShowUserInfo('maria', 48))
+*/
+
+//lo mismo pero con funciones flecha
+/*const ShowUserInfo = (userName, userAge) =>
+  `the username is ${userName}, the user is ${userAge} years old`;
+
+console.log(ShowUserInfo("fer", 50));
+console.log(ShowUserInfo("marcos", 35));
+console.log(ShowUserInfo("maria", 48));
+*/
