2ace064246807e5e4ac10de79d098a401f8e69ca
Fernando <fh1273509@mail.com>
2022-11-19 20:23:21 -0300

    module exports para objetos individuales

--- module/myModule.js
+++ module/myModule.js
@@ -1,5 +1,7 @@
 //los modulos se usan para aplicaciones grandes
 
+console.log(module);
+
 const myWebAddress = "programxf";
 const myNumber = 50;
 const myName = "Fernando";
@@ -7,10 +9,8 @@ const myLastName = "Herrera";
 
 //si esta variable  const la quiero reutilizar en el resto de la aplicacion la tengo que exportar
 
-const group = {
-    myWebAddress: myWebAddress,
-    myNumber: myNumber,
-    myName: myName,
-    myLastName: myLastName
-}
-module.exports = group;
+module.exports.name = myName;
+module.exports.lastname = myLastName;
+module.exports.number = myNumber;
+
+console.log(module);
