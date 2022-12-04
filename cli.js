82b786559fa331ce40923bba481e7070079f089d
Fernando <fh1273509@gmail.com>
2022-12-04 15:20:14 -0300

    npm instalando modulos

--- cli.js
+++ cli.js
@@ -4,3 +4,11 @@ const colors = require('colors');
 console.log("hola mundo".bgGreen);
 console.log("programfx".bgBlue);
 console.log("polvodeladrillo".bgYellow);
+
+//instalando varios modulos al mismo tiempo:
+//npm install lodash dayjs bootstrap
+
+//@popperjs es otro modulo que se instala automaticamente por que lo necesita bootstrap
+
+
+//en el package.json figura todos los modulos instalados
