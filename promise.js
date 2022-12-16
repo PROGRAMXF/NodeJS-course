6d05c6a9bb915de5ef0ef351d4533d9e51952c3d
Fernando <fh1273509@gmail.com>
2022-12-16 16:22:54 -0300

    promisify: modulo de utilidades de node

--- promise.js
+++ promise.js
@@ -1,47 +1,25 @@
 //vamos a leer un archivo de manera asincrona y la vamos a convertir en una promesa
 const { readFile } = require("fs");
-
-const getText = (pathFile) => {
-  //se va a ejecutar el codigo despues de un cierto tiempo a traves de una funcion
-  return new Promise(function (resolve, reject) {
-    readFile(pathFile, "utf-8", (err, data) => {
-      if (err) {
-        reject(err);
-      }
-      resolve(data);
-    }); //llamamos al archivo first y generamos cun callback
-  });
-};
-
-/*getText("./data/second.txt")
-    .then ((result) => console.log(result))  
-    .then (() => getText("./data/first.txt"))
-    .then (result => console.log(result))  
-    .catch(error => console.log(error))
-*/
+const {promisify} = require('util') //modulo de uilidades de node
+// con promisify puedo convertir un callback en una promesa sin escribir todo el código anterior con
+//solo escribir lo siguiente:
+const readFilePromise = promisify(readFile)
 
 
-
-
-//con async await le estamos diciendo que estamos manejando codigo asincrono
-//async await
 
 async function read() {
   //si quiero manejar lo errores coloco un try y luego un catch metiendo todo el codigo dentro:
 
-  try {
-//para probar el error vamos a generarlo con throw
+  try {   
 
-    throw new Error ("esto es un error")
-
-    const result = await getText("./data/first.txt");
+    const result = await readFilePromise("./data/first.txt", 'utf-8');
+    console.log(result);
     //en vez de usar un .then o .catch
     //y si quiero leer el siguiente archivo simplemente hago lo mismo:
-    const result2 = await getText("./data/second.txt");
-    //en vez de usar un .then o .catch
-
-    console.log(result);
+    const result2 = await readFilePromise("./data/second.txt", 'utf-8');
     console.log(result2);
+    //en vez de usar un .then o .catch    
+    
   } catch (error) {
     console.log(error);
   }
